95e7c220737374cc8417d5d00346d9abe021f33c
Ambassador Jane <info@fullcab.in>
2014-02-27 17:13:15 +0100

    lol

--- app/config.py
+++ app/config.py
@@ -2,11 +2,11 @@ config = {
     'password' : 'test',
     'secret' : 'secret'
 }
-mysql = {
-    'user' : 'root',
-    'password' : 'popelig...',
-    'host' : 'localhost',
-    'port' : 3306,
-    'db' : 'bfh'
-}
+#mysql = {
+#    'user' : 'root',
+#    'password' : 'password',
+#    'host' : 'localhost',
+#    'port' : 3306,
+#    'db' : 'name'
+#}
 sqlitedb = "/usr/local/nginx/www/traktor.de/app/cms.db"
